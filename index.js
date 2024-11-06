0db7363cf5ce265483f853b480cc1464e37e66bf
PTParra <49121638+PTParra@users.noreply.github.com>
2024-11-06 16:28:55 +0100

    commit inicial

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+    Redux Road
+    Now that we have a solid understanding of Redux’s core concepts, it’s time to put them into practice.
+
+    In this project, your objective is to create a text-based adventure game using reducers, state, and actions.
+    The state will serve as a reflection of the game’s current status, encompassing elements such as the player’s
+    inventory, distance covered, and time spent on the journey. Each occurrence within the game will be depicted as
+    an action, allowing players to collect supplies, embark on travels, and occasionally, if they dare, encounter
+    mishaps like overturning their wagon and losing their provisions.
+*/
+
